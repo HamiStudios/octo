3fe0496c3f8fa864ef8aba004bf106e73da7709c
Sean Hamilton <sean@hamistudios.com>
2018-10-01 18:52:24 +0100

    test: update and add tests

---
+++ tests/errors/NoErrorHandlingMethod.test.js
@@ -0,0 +1,13 @@
+// lib
+import NoErrorHandlingMethod from '../../src/errors/NoErrorHandlingMethod';
+import OctoErrorHandler from '../../src/ErrorHandler';
+
+test('NoErrorHandlingMethod should be a function', () => {
+  expect(typeof NoErrorHandlingMethod).toBe('function');
+});
+
+test('should return an Error instance', () => {
+  class TestErrorHandler extends OctoErrorHandler {}
+
+  expect(NoErrorHandlingMethod({ Instance: OctoErrorHandler })).toBeInstanceOf(Error);
+});
